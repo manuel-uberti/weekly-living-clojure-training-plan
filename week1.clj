850c7c4061381ebd8dbc304ac8fbefa4307070e8
Manuel Uberti <manuel@boccaperta.com>
2015-05-12 18:04:13 +0200

    Add day 3

--- week1.clj
+++ week1.clj
@@ -56,3 +56,38 @@
 (= 10 (let [x 4, y 6] (+ x y)))
 (= 4 (let [y 1, z 3] (+ y z)))
 (= 1 (let [z 1] z))
+
+;;; Day 3
+(= "ABC" (apply str (re-seq #"[A-Z]+" "bA1B3Ce")))
+
+(= '(5 4 3 2 1) ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5))
+
+(= [7 6 5 4 3]
+   (loop [x 5
+          result []]
+     (if (> x 0)
+       (recur (dec x) (conj result (+ 2 x)))
+       result)))
+
+(= (last (sort (rest (reverse [2 5 4 1 3 6]))))
+   (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (last))
+   5)
+
+(= (reduce + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+   (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (reduce +))
+   11)
+
+(= '(1 5 9 13 17 21 25 29 33 37)
+   (for [x (range 40)
+         :when (= 1 (rem x 4))]
+     x))
+
+(= '(1 5 9 13 17 21 25 29 33 37)
+   (for [x (iterate #(+ 4 %) 0)
+         :let [z (inc x)]
+         :while (< z 40)]
+     z))
+
+(= '(1 5 9 13 17 21 25 29 33 37)
+   (for [[x y] (partition 2 (range 20))]
+     (+ x y)))
