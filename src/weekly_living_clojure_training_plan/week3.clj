953fb8243b29ad931ff463479083c71500460d3a
Manuel Uberti <manuel@boccaperta.com>
2015-05-27 17:23:49 +0200

    Add day 3

--- src/weekly_living_clojure_training_plan/week3.clj
+++ src/weekly_living_clojure_training_plan/week3.clj
@@ -102,3 +102,25 @@
 (= (rotate 1 '(:a :b :c)) '(:b :c :a))
 
 (= (rotate -4 '(:a :b :c)) '(:c :a :b))
+
+
+;;; Day 3
+;; Reverse Interleave
+(defn r-interleave [s x]
+  (apply map list (partition x s)))
+
+(= (r-interleave [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
+
+(= (r-interleave (range 9) 3) '((0 3 6) (1 4 7) (2 5 8)))
+
+(= (r-interleave (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))
+
+;; Split by Type
+(defn split-by-type [s]
+  (vals (group-by #(type %) s)))
+
+(= (set (split-by-type [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]})
+
+(= (set (split-by-type [:a "foo" "bar" :b])) #{[:a :b] ["foo" "bar"]})
+
+(= (set (split-by-type [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]})
